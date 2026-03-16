4c1f7f489721a58f4d32dd99ce6d27057961632f
lorenzopiic <tua@email.com>
2026-03-16 21:57:51 +0100

    ok

--- hashing/hash_table.c
+++ hashing/hash_table.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include"hash_table.h"
+#include<math.h>
 #include<string.h>
 
+
+int main(void){
+
+	hashtable* ht = create_table();
+   	delete_hash_table(ht);	
+}
+	
 static record* 
-create_record (const char* k, const char* v) 
-{
+create_record (const char* k, const char* v){
 	record* newPtr = malloc(sizeof(*newPtr));
 	if(newPtr == NULL) { return NULL; }
 	newPtr->key = malloc(strlen(k)+1); 
@@ -24,8 +31,7 @@ create_record (const char* k, const char* v)
 }
 
 static hashtable* 
-create_table()
-{
+create_table(){
 	hashtable* ht = malloc(sizeof(*ht));
 	if(ht == NULL) return NULL; 
 	ht->capacity = 53; 
@@ -37,8 +43,7 @@ create_table()
 } 
 
 static void
-delete_record (record* rec) 
-{
+delete_record (record* rec){
     free(rec->key);
     free(rec->value);
     free(rec);
@@ -46,9 +51,8 @@ delete_record (record* rec)
 
 
 void 
-delete_hash_table (hashtable* ht) 
-{
- 	for (size_t i = 0; i < ht->size; i++) {
+delete_hash_table (hashtable* ht){
+ 	for (size_t i = 0; i < ht->capacity; i++) {
         record* temp = ht->buckets[i];
         if (temp != NULL) {
             delete_record(temp);
@@ -56,4 +60,35 @@ delete_hash_table (hashtable* ht)
     }
     free(ht->buckets);
     free(ht);
+}
+
+unsigned long
+hash(const char* s, const int a, const int m){
+	unsigned long hash = 0; 
+	for(size_t i=0; i<strlen(s); i++){
+		hash+= pow(a,strlen(s)-(i+1))*s[i];
+		hash = hash % m;
+	}
+	return hash;
+}
+
+static int
+get_hash(const char* s, const int num_buckets, const int attempt){
+    const int hash_a = hash(s, HT_PRIME_1, num_buckets);
+    const int hash_b = hash(s, HT_PRIME_2, num_buckets);
+    return (hash_a + (attempt * (hash_b + 1))) % num_buckets;
+}
+
+void insert(hashtable* ht, const char* key, const char* value){
+	record* newPtr = create_record(key,value);
+	int index = get_hash(record->key,ht->capacity,0);
+	record* current_record = ht->buckets[index];
+	int i = 1; 
+	while(current_record != NULL){
+		index = get_hash(record->key,ht->capacity,i);
+		current_record = ht->buckets[index];	
+		i++;
+	}
+	ht->buckets[index] = newPtr;
+	ht->count++;
 }
--- hashing/hash_table.h
+++ hashing/hash_table.h
@@ -12,6 +12,9 @@ typedef struct{
  }hashtable; 
 
 
-
-
-
+static record* create_record (const char* k, const char* v);
+static hashtable* create_table();
+static void delete_record (record* rec);
+void delete_hash_table (hashtable* ht);
+unsigned long hash(const char* s, const int a, const int m);
+static int get_hash(const char* s, const int num_buckets, const int attempt);
